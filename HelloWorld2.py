af1b4fdbe5c227a1aa885a636323434401edef37
gongbinglai <gongbinglai@126.com>
2019-02-12 16:54:14 +0800

    python
    python

---
+++ HelloWorld2.py
@@ -0,0 +1,67 @@
+# print("hello world")
+
+
+# name = "gongbinglai"
+#
+# print("hello "+name.upper())
+#
+#
+# nameList = ["gongbinglai"]
+#
+#
+# nameList.append("hdclai") # 在最后添加
+# nameList.insert(0,"zhangsan") # 在第一个位置添加
+#
+# nameList[0] = "lisi"  # 修改第一个位置的内容
+#
+# nameList.remove("lisi") # 删除lisi
+#
+# print(nameList)
+#
+#
+# print("lisi" not in nameList)
+#
+#
+# popName = nameList.pop(1)  #删除指定位置的元素
+#
+# print(popName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
